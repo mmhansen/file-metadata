b19d8a8d986d052805f484a81b55c53bb36ead34
Michael Hansen <hansen.mmb@gmail.com>
2016-09-30 08:31:09 -0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+##File metadata microservice  
+User Stories  
+1. I can submit a FormData object that includes a file upload.   
+2. When I submit something, I will receive the file size in bytes within the JSON response.   
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    assert = require('assert'),
+    multer = require('multer'),
+    app = express(),
+    upload = multer();
+
+app.use(express.static("./public"));
+
+app.post('/upload', upload.single('file'), function(req, res){
+  res.json({
+    "name":req.file.originalname,
+    "size":req.file.size+" bytes"
+  })
+})
+
+app.listen(process.env.port || 3000, function(){
+  console.log("App listening on port 3000")
+})
